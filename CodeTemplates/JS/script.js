ade8f473b32658a1b0917ea7c4c4760855d08795
EvanWilliams5352 <143012664+EvanWilliams5352@users.noreply.github.com>
2023-11-13 11:33:25 -0700

    javascript challenge

--- CodeTemplates/JS/script.js
+++ CodeTemplates/JS/script.js
@@ -1,1 +1,15 @@
-// JavaScript source code
+function toggleTextContent(index) {
+    const textContent = document.querySelector('.text-content');
+    const labels = document.querySelectorAll('.hover-label');
+
+    // Hide all labels
+    labels.forEach(label => {
+        label.style.opacity = 0;
+    });
+
+    // Show the label corresponding to the clicked circle
+    labels[index - 1].style.opacity = 1;
+
+    // You can customize this part based on your requirements, e.g., toggle visibility of text content
+    textContent.style.display = (textContent.style.display === 'none' || textContent.style.display === '') ? 'block' : 'none';
+}
